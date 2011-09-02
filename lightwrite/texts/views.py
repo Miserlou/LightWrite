36813dead418f4799431d3c7f8443d1175782e63
Rich Jones <rich@anomos.info>
2011-09-02 01:11:28 -0400

    save to file

--- lightwrite/texts/views.py
+++ lightwrite/texts/views.py
@@ -18,9 +18,6 @@ def root(request):
 def write(request, wash):
 
     if request.method == 'POST':
-
-        print request.POST
-
         t = Text.objects.filter(wash=wash)
         if (len(t)==0):
             t = Text(wash=wash)
@@ -35,28 +32,11 @@ def write(request, wash):
         if 'save' in request.POST:
             return render_to_response('light.html', {'wash': wash,}, context_instance=RequestContext(request))
         else:
-            return HttpResponse(t[0].text, mimetype='text/plain')
+            resp = HttpResponse(t[0].text.strip(), mimetype='text/plain')
+            resp['Content-Disposition'] = 'attachment; filename=' + wash + '.txt'
+            return resp
  
     return render_to_response('light.html', {'wash': wash,}, context_instance=RequestContext(request))
-
-def file(request, wash):
-
-    print request.method
-
-    if request.method == 'POST':
-        t = Text.objects.filter(wash=wash)
-        if (len(t)==0):
-            t = Text(wash=wash)
-            t.text = request.POST['writearea']
-            t.save() 
-            t = Text.objects.filter(wash=wash)
-        else:
-            t = t[0] 
-            t.text = request.POST['writearea']
-            t.save() 
-            t = Text.objects.filter(wash=wash)
-        
-    return HttpResponse(t.text, mimetype='text/plain')
 
 def json_get_text(request, wash):
     t = Text.objects.filter(wash=wash)
