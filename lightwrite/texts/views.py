9f6467d35a56f0a49f611cec3afe5e12ac192595
Rich Jones <rich@anomos.info>
2011-09-02 00:31:52 -0400

    g fresh

--- lightwrite/texts/views.py
+++ lightwrite/texts/views.py
@@ -16,13 +16,28 @@ def root(request):
     return HttpResponseRedirect('/' + wash) 
 
 def write(request, wash):
-    return render_to_response('light.html', {'wash': wash,})
+
+    if request.method == 'POST':
+        t = Text.objects.filter(wash=wash)
+        if (len(t)==0):
+            t = Text(wash=wash)
+            t.text = request.POST['writearea']
+            t.save() 
+            t = Text.objects.filter(wash=wash)
+        else:
+            t = t[0] 
+            t.text = request.POST['writearea']
+            t.save() 
+            t = Text.objects.filter(wash=wash)
+        
+    return render_to_response('light.html', {'wash': wash,}, context_instance=RequestContext(request))
 
 def json_get_text(request, wash):
     t = Text.objects.filter(wash=wash)
     if (len(t)==0):
         t = Text(wash=wash)
-        t.text = '\n\nWelcome to LightWrite!\nby Gun.io\n\nLightWrite is a web-based clone of WriteRoom - it\'s a place to write your thoughts without any distractions.\n\nTo use LightWrite, just start typing! You can move your mouse over the bottom of the page to save your text or export to a file. \n\nTo enter fullscreen mode, just press F11.'
+        t.text = '\n\nWelcome to LightWrite!\nby Gun.io\n\nLightWrite is a web-based clone of WriteRoom - it\'s a place to write your thoughts without any distractions.\n\nTo use LightWrite, just start typing! You can move your mouse over the bottom of the page to save your text online or export to a file. \n\nTo enter fullscreen mode, just press F11.'
         t.save() 
+        t = Text.objects.filter(wash=wash)
     data = serializers.serialize("json", t)
     return HttpResponse(data, mimetype='application/json')
